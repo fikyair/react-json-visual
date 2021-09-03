ecd3129b6c7cff1e44b4f880905cf51fa7fdcc00
fikyair <shiming_xsm@163.com>
2021-09-04 00:01:56 +0800

    first commit

---
+++ config/webpack.dev.js
@@ -0,0 +1,55 @@
+const webpack = require('webpack')
+const { merge } = require('webpack-merge')
+
+const common = require('./webpack.common.js')
+const paths = require('./paths')
+
+module.exports = merge(common, {
+  // Set the mode to development or production
+  mode: 'development',
+
+  // Control how source maps are generated
+  devtool: 'inline-source-map',
+
+  // Spin up a server for quick development
+  devServer: {
+    historyApiFallback: true,
+    // contentBase: paths.build,
+    open: true,
+    compress: true,
+    hot: true,
+    port: 8080,
+  },
+
+  module: {
+    rules: [
+      // Styles: Inject CSS into the head with source maps
+      {
+        test: /\.(less|css)$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+              importLoaders: 1,
+              // 开启了 css modules 后，只针对 css modules 生效
+              modules: {
+                mode: 'local',
+                localIdentName: '[path][name]__[local]--[hash:base64:5]',
+              },
+              url: true,
+            },
+          },
+          { loader: 'postcss-loader', options: { sourceMap: true } },
+          { loader: 'less-loader', options: { sourceMap: true } },
+        ],
+      },
+    ],
+  },
+
+  plugins: [
+    // Only update what has changed on hot reload
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+})
